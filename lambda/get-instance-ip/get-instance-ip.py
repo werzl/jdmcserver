0f54dfa11faf40e114a86c96ce28857b6476a250
JoeDurrant <josephwdurrant@gmail.com>
2020-04-13 21:19:54 +0100

    Filter instances based on server instance-id

--- lambda/get-instance-ip/get-instance-ip.py
+++ lambda/get-instance-ip/get-instance-ip.py
@@ -3,8 +3,11 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2')
-    response = ec2.describe_instances()
+    #region = os.environ['region']
+    region = "eu-west-2"
+    ec2 = boto3.client('ec2', region_name=region)
+    #instance_ids = [os.environ['instanceId']]
+    response = ec2.describe_instances(InstanceIds=['i-029f683986573a67e',])
     instance_details = {}
     instance_details['launchTime'] = response['Reservations'][0]['Instances'][0]['LaunchTime'].strftime('%d/%m/%y %I:%M %S %p')
     instance_details['availabilityZone'] = response['Reservations'][0]['Instances'][0]['Placement']['AvailabilityZone']
@@ -19,3 +22,4 @@ def lambda_handler(event, context):
                 "Access-Control-Allow-Credentials" : True
             }
         }
+print(lambda_handler("",""))
