7bae59b6464fa58656a7c784992c4dccd031c790
werzl <adam@houseofhewitt.co.uk>
2020-10-25 16:50:59 +0000

    Update get-instance-details.py

--- lambda/get-instance-details/get-instance-details.py
+++ lambda/get-instance-details/get-instance-details.py
@@ -13,7 +13,7 @@ def lambda_handler(event, context):
 
     response = ec2.describe_instances(InstanceIds=[instance_ids])
     instance_details = {}
-    instance_details['launchTime'] = response['Reservations'][0]['Instances'][0]['LaunchTime'].strftime('%d/%m/%y %I:%M %S %p')
+    instance_details['launchTime'] = response['Reservations'][0]['Instances'][0]['LaunchTime'].strftime('%Y-%m-%dT%H:%M:%SZ')
     instance_details['availabilityZone'] = response['Reservations'][0]['Instances'][0]['Placement']['AvailabilityZone']
     instance_details['publicDnsName'] = response['Reservations'][0]['Instances'][0]['PublicDnsName']
     instance_details['publicIpAddress'] = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
