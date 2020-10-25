4b17ea3e050c2a0aff3c6d46220680028e444a73
JoeDurrant <josephwdurrant@gmail.com>
2020-10-25 18:10:40 +0000

    Fix s3 backup script

--- ec2/s3-backup.sh
+++ ec2/s3-backup.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-#This script will take your world files and upload them to a specified S3 bucket.
-#Requires AWS CLI and bucket permissions (would recommend using an IAM role)
-#Any previous backup will be moved to the subdirectory "old" to give a copy of the world before and after starting the server.
-aws s3 mv s3://BUCKET-NAME/world s3://BUCKET-NAME/old --recursive
-aws s3 cp path/to/world/folder/ s3://BUCKET-NAME/world --recursive
+!/bin/bash
+#This script will sync your world with an S3 bucket whenever the world is stopped. Make sure to update the values.
+aws s3 sync /path/to/world/directory/ s3://bucket-name
